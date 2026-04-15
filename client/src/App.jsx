e207fe749fb1933d90cfe91ca8b853215c4b109b
KishorPSS <aryank@parasightsolutions.com>
2026-04-15 13:11:34 +0530

    -

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 function App() {
-  return <div className="text-blue-500">hii</div>;
+  return <div className="text-blue-500">Job Portal</div>;
 }
 
 export default App;
